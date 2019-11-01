a724dafd48fc026338abe55365639e1c6803173c
Suresh Tiwari <sureshtiwari1993@gmail.com>
2019-11-01 12:38:39 +0530

    Program to find the majority element by Using hashmap and adding frequency to hashmap
    Change-Id: Iccb651bc3b085867717564499ba408a4c3769956
    Signed-off-by: Suresh Tiwari <sureshtiwari1993@gmail.com>

--- src/com/programs/Solution1.java
+++ src/com/programs/Solution1.java
@@ -6,6 +6,7 @@ import java.math.*;
 import java.security.*;
 import java.text.*;
 import java.util.*;
+import java.util.Map.Entry;
 import java.util.concurrent.*;
 import java.util.function.*;
 import java.util.regex.*;
@@ -14,56 +15,35 @@ import static java.util.stream.Collectors.joining;
 import static java.util.stream.Collectors.toList;
 
 public class Solution1 {
-
-	static int makeAnagram(String a, String b) {
-		HashMap<Character, Integer> astring = new HashMap<Character, Integer>();
-		HashMap<Character, Integer> bstring = new HashMap<Character, Integer>();
-        for(int i=0;i<a.length();i++)
-        	if(astring.containsKey(a.charAt(i)))
-        		astring.put(a.charAt(i), astring.get(a.charAt(i))+1);
-        	else
-        		astring.put(a.charAt(i), 1);
-        
-        for(int i=0;i<b.length();i++)
-        	if(bstring.containsKey(b.charAt(i)))
-        		bstring.put(b.charAt(i), bstring.get(b.charAt(i))+1);
-        	else
-        		bstring.put(b.charAt(i), 1);
-        
-        for(int i=0;i<b.length();i++)
-        {
-        	char c = b.charAt(i);
-        	if(astring.containsKey(c))
-        	{
-        		if(astring.get(c)==1)
-        			astring.remove(c);
-        		else
-        			astring.put(c, astring.get(c)-1);
-        		
-        		if(bstring.get(c)==1)
-        			bstring.remove(c);
-        		else
-        			bstring.put(c, bstring.get(c)-1);
-        		
-        	}
-        }
-        Collection<Integer> temp =astring.values();
-        Iterator<Integer> iterator = temp.iterator();
-        int result = 0;
-        while (iterator.hasNext()) 
-        result = result + iterator.next();
-        temp =bstring.values();
-        iterator = temp.iterator();
-        while (iterator.hasNext()) 
-            result = result + iterator.next();
-        
-        return result;
-    }
+	
+	 public static int majorityElement(int[] nums) {
+	        HashMap<Integer, Integer> hm = new HashMap<Integer, Integer>();
+	        for(int i=0;i<nums.length;i++)
+	        {
+	        	if(hm.containsKey(nums[i]))
+	        	{
+	        		hm.put(nums[i], hm.get(nums[i])+1);
+	        	}
+	        	else
+	        	{
+	        		hm.put(nums[i], 1);
+	        	}
+	        }
+	        int answer=0;
+	        int result = 0;
+	        for(Map.Entry<Integer, Integer> entry : hm.entrySet()) {
+			    if(entry.getValue()>result)
+			    {
+			    	answer = entry.getKey();
+			    	result=entry.getValue();
+			    }
+			}
+	        return answer;
+	    }
 
 	public static void main(String[] args) throws IOException {
-		String a = null;
-		String b = null;
-		System.out.println(makeAnagram(a,b));
+		int[] a = {3,3,4};
+		System.out.println(majorityElement(a));
 
 	}
 }
